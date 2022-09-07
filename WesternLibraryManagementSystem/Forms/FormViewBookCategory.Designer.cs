6094b9911ea171eece017680df230d30c1ae3e0b
Angkor-Rhapsody <77953604+Angkor-Rhapsody@users.noreply.github.com>
2022-09-08 06:28:41 +0700

    Add Panels
    Three panels was created. One panel for all over the form and the other panel for put data grid view and the last one for put buttons

--- WesternLibraryManagementSystem/Forms/FormViewBookCategory.Designer.cs
+++ WesternLibraryManagementSystem/Forms/FormViewBookCategory.Designer.cs
@@ -28,12 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1008, 600);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(122, 54);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(557, 508);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(764, 104);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(170, 412);
+            this.panel3.TabIndex = 1;
+            // 
+            // FormViewBookCategory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1005, 600);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormViewBookCategory";
             this.Text = "FormViewBookCategory";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
     }
 }
