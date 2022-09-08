8554f8be5d3636874a9b08613c614de394b471a1
Angkor-Rhapsody <77953604+Angkor-Rhapsody@users.noreply.github.com>
2022-09-08 08:10:16 +0700

    rename Datagridview and label name

--- WesternLibraryManagementSystem/Forms/FormViewBookCategory.Designer.cs
+++ WesternLibraryManagementSystem/Forms/FormViewBookCategory.Designer.cs
@@ -29,18 +29,18 @@
         private void InitializeComponent()
         {
             this.panelMain = new System.Windows.Forms.Panel();
-            this.panelViewCategory = new System.Windows.Forms.Panel();
-            this.panelButton = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.panelButton = new System.Windows.Forms.Panel();
             this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelViewCategory = new System.Windows.Forms.Panel();
+            this.dgvBookCategories = new System.Windows.Forms.DataGridView();
             this.panelMain.SuspendLayout();
-            this.panelViewCategory.SuspendLayout();
             this.panelButton.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panelViewCategory.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBookCategories)).BeginInit();
             this.SuspendLayout();
             // 
             // panelMain
@@ -53,13 +53,14 @@
             this.panelMain.Size = new System.Drawing.Size(1008, 600);
             this.panelMain.TabIndex = 0;
             // 
-            // panelViewCategory
+            // label1
             // 
-            this.panelViewCategory.Controls.Add(this.dataGridView1);
-            this.panelViewCategory.Location = new System.Drawing.Point(122, 65);
-            this.panelViewCategory.Name = "panelViewCategory";
-            this.panelViewCategory.Size = new System.Drawing.Size(557, 508);
-            this.panelViewCategory.TabIndex = 0;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(380, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "View Categories";
             // 
             // panelButton
             // 
@@ -72,42 +73,15 @@
             this.panelButton.Size = new System.Drawing.Size(170, 373);
             this.panelButton.TabIndex = 1;
             // 
-            // label1
+            // button4
             // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(380, 10);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(190, 37);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(30, 46);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 67);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(19, 17);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(513, 480);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(30, 119);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(105, 67);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(30, 265);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 67);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
@@ -119,15 +93,41 @@
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // button2
             // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(30, 265);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(105, 67);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(30, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 67);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(30, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panelViewCategory
+            // 
+            this.panelViewCategory.Controls.Add(this.dgvBookCategories);
+            this.panelViewCategory.Location = new System.Drawing.Point(122, 65);
+            this.panelViewCategory.Name = "panelViewCategory";
+            this.panelViewCategory.Size = new System.Drawing.Size(557, 508);
+            this.panelViewCategory.TabIndex = 0;
+            // 
+            // dgvBookCategories
+            // 
+            this.dgvBookCategories.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBookCategories.Location = new System.Drawing.Point(19, 17);
+            this.dgvBookCategories.Name = "dgvBookCategories";
+            this.dgvBookCategories.Size = new System.Drawing.Size(513, 480);
+            this.dgvBookCategories.TabIndex = 0;
             // 
             // FormViewBookCategory
             // 
@@ -138,9 +138,9 @@
             this.Name = "FormViewBookCategory";
             this.Text = "FormViewBookCategory";
             this.panelMain.ResumeLayout(false);
-            this.panelViewCategory.ResumeLayout(false);
             this.panelButton.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panelViewCategory.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBookCategories)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -152,7 +152,7 @@
         private System.Windows.Forms.Panel panelViewCategory;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvBookCategories;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
