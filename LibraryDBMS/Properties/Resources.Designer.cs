74778d23a639603bbe1bbcf23a4b4822ba47e7e4
kim-chimy <106795190+kim-chimy@users.noreply.github.com>
2022-10-02 08:15:10 +0700

    - Rename project and assembly name

--- LibraryDBMS/Properties/Resources.Designer.cs
+++ LibraryDBMS/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WesternLibraryManagementSystem.Properties {
+namespace LibraryDBMS.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WesternLibraryManagementSystem.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WesternLibraryManagementSystem.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LibraryDBMS.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
